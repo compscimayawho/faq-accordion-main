046d91e071799c6c157f21037bf251837c5d84f3
compscimayawho <99605117+compscimayawho@users.noreply.github.com>
2024-04-27 10:48:52 +0300

    Edit JS

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const icons = document.querySelectorAll('.icon');
 const answers = document.querySelectorAll('.question-answer');
 const source = [
-    'https://github.com/compscimayawho/faq-accordion-main/blob/main/images/icon-plus.svg',
-    'https://github.com/compscimayawho/faq-accordion-main/blob/main/images/icon-minus.svg'
+    'https://raw.githubusercontent.com/compscimayawho/faq-accordion-main/main/images/icon-plus.svg?token=GHSAT0AAAAAACRFTVGZWVP2RHWOCOKTB6IEZRMXMUQ',
+    'https://raw.githubusercontent.com/compscimayawho/faq-accordion-main/main/images/icon-minus.svg?token=GHSAT0AAAAAACRFTVGZ5GODRVHOGW45E3D2ZRMXMEQ'
 ];
 
 for (let i = 0; i < icons.length; i++) {
     icons[i].addEventListener('click', () => {
         answers[i].classList.toggle('hide');
         let str = String(icons[i].src);
-        if (str.contains('/icon-plus')) {
+        if (str.includes('/images/icon-plus')) {
             icons[i].src = source[1];
         } else {
             icons[i].src = source[0];
